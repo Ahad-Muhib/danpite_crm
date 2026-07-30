94a327fd075318fa824ca62ac741cee545931272
Ahad-Muhib <muhibalahad@gmail.com>
2026-07-30 19:55:15 +0600

    report start1

--- danpite_crm/settings.py
+++ danpite_crm/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-danpite-crm-secret-key-change-in-production-2024'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
